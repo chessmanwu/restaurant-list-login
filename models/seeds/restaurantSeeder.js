d2a660a19cbec24d9db38c3b047350b442930706
chessmanwu <chessman@hotmail.com.tw>
2021-07-11 23:13:37 +0800

    feat: add seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,39 @@
+// mongoose
+const mongoose = require('mongoose')
+// 載入 restaurant list 種子資料
+const data = require('../../restaurant.json')
+const restaurantList = data.results
+// restaurant model
+const Restaurant = require('../restaurant')
+
+// 設定連線到 mongoDB
+mongoose.connect('mongodb://localhost/restaurant-list', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected')
+  // 建立資料
+  restaurantList.forEach((restaurant) => {
+    Restaurant.create({
+      name: restaurant.name,
+      name_en: restaurant.name_en,
+      category: restaurant.category,
+      image: restaurant.image,
+      location: restaurant.location,
+      phone: restaurant.phone,
+      google_map: restaurant.google_map,
+      rating: restaurant.rating,
+      description: restaurant.description,
+    })
+  })
+  console.log('restaurantSeeder done!')
+})
