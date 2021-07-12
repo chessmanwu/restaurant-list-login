f675d0e55ab2e23bf7f6b806f6921c5fc0453fe4
chessmanwu <chessman@hotmail.com.tw>
2021-07-13 00:49:39 +0800

    new

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 const port = 3000
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
 
 const Restaurant = require('./models/restaurant')
-const restaurants = require('./restaurant.json')
+// const restaurants = require('./restaurant.json')
 
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoD
 
@@ -20,19 +20,46 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+const categories = new Set()
+Restaurant.find().lean().then((restaurants) => {
+  restaurants.forEach(restaurant =>
+    categories.add(restaurant.category))
+})
+
+
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 // setting static files
-app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 // routes setting
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
-    .then(restaurants=> res.render('index', { restaurants }))
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
+})
+
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new', { category: categories })
+})
+
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
@@ -56,6 +83,8 @@ app.get('/search', (req, res) => {
 
   res.render('index', { restaurant: restaurant, keyword: keyword })
 })
+
+app.use(express.static('public'))
 
 // start and listen on the Express server
 app.listen(port, () => {
